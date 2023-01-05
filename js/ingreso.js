6692880de7183f744eda5a98e0fb2cfeb6334acb
ewervallejo <ewer1980@hotmail.com>
2023-01-04 19:32:40 -0500

    Version 1

---
+++ js/ingreso.js
@@ -0,0 +1,18 @@
+/*const auto = {
+    marca: "Chevrolet",
+    modelo: 2020,
+    accesorios:['llantas', 'volante', 'direccionales']
+
+}
+console.log(auto.modelo);
+
+
+for (let i = 0; i<= 9; i++){
+    console.log(i);
+
+}*/
+let nombre, mensaje;
+
+nombre = window.prompt("¿cual es tu nombre?");
+mensaje = 'Hola como estas ${nombre}';
+document.write(mensaje);
---
+++ js/inicio.js
@@ -0,0 +1,17 @@
+
+let nombre = "Beto";
+let edad = 23;
+
+console.log("Mi nombre es" + nombre + " y Mi edad es" + edad + " años ");
+
+console.log(nombre.length)
+console.log(nombre.indexOf('t'));
+console-log(nombre.replace('Beto','Bryan'));
+
+let num1 = "2";
+let num2 = "6";
+console.log(num1 + num2);
+console.log(Number(num1)+ Number(num2));
+
+
+
